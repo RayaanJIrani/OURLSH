3b757aecfa043a42aa8d8b6b00c601597a943d57
lukedemaio <demaioluke22@gmail.com>
2022-10-31 16:52:16 -0500

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -5,10 +5,12 @@ const LandlordController = require('../controllers/landlord');
 
 router.post('/tenant', async (req, res, next) => {
    try {
-       const body = req.body;
+        const body = req.body;
       
-       const result = await authenticateTenant(body.email, body.password);
-       res.status(200).json(result);
+        const result = await authenticateTenant(body.email, body.password);
+        if(result != null){
+            res.status(200).json(result);
+        }
    } catch (err) {
        console.error('Failed to authenticate user:', err);
        res.status(401).json({ message: err.toString() });
@@ -21,11 +23,25 @@ router.post('/landlord', async (req, res, next) => {
         const body = req.body;
        
         const result = await authenticateLandlord(body.email, body.password);
-        res.status(200).json(result);
+        if(result != null){
+            res.status(200).json(result);
+        }
     } catch (err) {
         console.error('Failed to authenticate user:', err);
         res.status(401).json({ message: err.toString() });
     }
     next();
  })
+//  router.post('/landlord', async (req, res, next) => {
+//     try {
+//         const body = req.body;
+       
+//         const result = await authenticateLandlord(body.email, body.password);
+//         res.status(200).json(result);
+//     } catch (err) {
+//         console.error('Failed to authenticate user:', err);
+//         res.status(401).json({ message: err.toString() });
+//     }
+//     next();
+//  })
 module.exports = router;
