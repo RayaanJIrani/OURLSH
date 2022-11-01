a024cc43ef21bac456a3c54228926f51eb48d8be
wesoa012 <wesa@smu.edu>
2022-10-31 22:09:10 -0500

    Same as tenant get by id but now for landlord

---
+++ backend/routes/landlord.js
@@ -0,0 +1,32 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+router = express.Router();
+router.use(bodyParser.json());
+
+
+
+router.get('/:id', async (req, res, next) => {
+    let accountId = parseInt(req.params.id);
+    //let accountId = req.query.id; //Don't know whether params or body will be used
+    if(typeof(accountId) !== 'number' || !accountId)
+    {
+        console.log("account_id is not type Number or is 0");
+        res.status(400).send();
+    }
+    else{
+        const tenantByID = await req.models.landlord.fetchLandlordByID(accountId);
+        //will return 404 not found if id does not exist
+        if(JSON.stringify(tenantByID) == '[]')
+        {
+            console.log("No user found with id", accountId)
+            res.status(404).send();
+        }
+        else
+        {
+            res.json(tenantByID);
+        }
+    }
+    next();
+});
+
+module.exports = router;
