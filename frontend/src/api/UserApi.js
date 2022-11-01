7ece937c5b295463082f8b1e518c7f324a072fcb
ziyusunmao <78039863+ziyusunmao@users.noreply.github.com>
2022-10-31 22:11:01 -0500

    update

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -26,22 +26,7 @@ export const checkAccount = (email,password) =>new Promise((resolve, reject) =>{
 
 
 // tenant profile
-// export const getTenantInfo = () => new Promise((resolve, reject) => {
-//   let apiConfig = {
-//     headers: {
-//       Authorization: "Bearer " + localStorage.getItem("token"),//login放token如local storage，我再取
-//     },
-//   };
-//   axios.get(`${baseEndpoint}/tenants`, apiConfig)
-//     .then((x) => resolve(x.data))
-//     .catch((x) => {
-//       alert(x);
-//       reject(x);
-//     });
-// });
-
 export const getTenantInfo = (id) => new Promise((resolve, reject) => {
-  id = 1
     let apiConfig = {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),//login放token如local storage，我再取
@@ -58,14 +43,13 @@ export const getTenantInfo = (id) => new Promise((resolve, reject) => {
 
 
   export const getLandlordInfo = (id) => new Promise((resolve, reject) => {
-    id = 1
       let apiConfig = {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token"),//login放token如local storage，我再取
         },
       };
       axios
-        .get(`${baseEndpoint}/landlord/${id}`, apiConfig)
+        .get(`${baseEndpoint}/landlords/${id}`, apiConfig)
         .then((x) => resolve(x.data))
         .catch((x) => {
           alert(x);
--- frontend/src/profile/tenantProfile.jsx
+++ frontend/src/profile/tenantProfile.jsx
@@ -11,7 +11,7 @@ export const TenantProfile = () => {
   //   const [photo, setPhoto] = useState("");
   //   const [update, setUpdate] = useState(false);
   useEffect(() => {
-    getTenantInfo().then((x) => setTenant(x));
+    getTenantInfo(tenant[0].prop_id).then((x) => setTenant(x));
   }, []);
 
   if (!tenant) { //在加载期间显示
