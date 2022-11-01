bc8e87b5819e33083fb8d05ed068cec6ebf2f8d7
wesoa012 <wesa@smu.edu>
2022-10-31 22:08:47 -0500

    formatting update

--- backend/models/tenant.js
+++ backend/models/tenant.js
@@ -5,11 +5,11 @@ const TENANT_TABLE = 'tenant';
       //uses passed in id to get the associated tenant but only the specified columns
        const query = knex(TENANT_TABLE).where({ id })
        .select("id",
-       "email",
-       "first_name",
-       "last_name",
-       "prop_id",
-       "landlord_id");
+               "email",
+               "first_name",
+               "last_name",
+               "prop_id",
+               "landlord_id");
 
        const results = await query;
        return results;
@@ -18,12 +18,12 @@ const TENANT_TABLE = 'tenant';
    //for loginpurposes
    const loginFetchTenantByEmail = async (email) => {
       //uses passed in id to get the associated tenant but only the specified columns
-       const query = knex(TENANT_TABLE).where({ email }).select(
-         "email",
-         "first_name",
-         "last_name",
-         "password"
-       );
+       const query = knex(TENANT_TABLE).where({ email })
+       .select("email",
+               "first_name",
+               "last_name",
+               "password");
+               
        const results = await query;
        return results;
    }
