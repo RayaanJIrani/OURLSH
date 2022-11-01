0ba652c39d07a9dce4b2f87398f41f477ba7b73d
wesoa012 <wesa@smu.edu>
2022-10-31 21:48:37 -0500

    Correcting bugs
    Need to send in the request to authenticate, otherwise the functions break.
    
    If statement also corrected. Was relying on the err to be caught to return a 401 (email/pass dont match), however the err is only caught when the request doesn't go through, so in the case we have, the request go through, it catches the bad login attempt

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,15 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const { authenticateLandlord, authenticateTenant } = require('../models/login');
 router = express.Router();
 router.use(bodyParser.json());
 
 router.post('/tenant', async (req, res, next) => {
    try {
         const body = req.body;
-      
-        const result = await authenticateTenant(body.email, body.password);
+        const result = await authenticateTenant(req, body.email, body.password);
         if(result != null){
             res.status(200).json(result);
+        }
+        else
+        {
+            res.status(401).json("Password does not match the email given"); 
         }
    } catch (err) {
        console.error('Failed to authenticate user:', err);
@@ -21,10 +25,13 @@ router.post('/tenant', async (req, res, next) => {
 router.post('/landlord', async (req, res, next) => {
     try {
         const body = req.body;
-       
-        const result = await authenticateLandlord(body.email, body.password);
+        const result = await authenticateLandlord(req, body.email, body.password);
         if(result != null){
             res.status(200).json(result);//added if statement
+        }
+        else
+        {
+            res.status(401).json("Password does not match the email given"); 
         }
     } catch (err) {
         console.error('Failed to authenticate user:', err);
