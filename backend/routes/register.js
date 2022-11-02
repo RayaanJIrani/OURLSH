5e84d5785e4ff5c385155b0aa237a17ee3685042
wesoa012 <wesa@smu.edu>
2022-11-01 19:03:00 -0500

    Register will now make sure users have unique emails
    A tenant and landlord may have the same email, however 2 tenants may not share same and 2 landlords should not share

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -10,42 +10,71 @@ router.post('/tenant', async (req, res, next) => {
     let password = req.body.password;
     let first_name = req.body.first_name;
     let last_name = req.body.last_name;
-    
-    if (email === undefined || password === undefined || first_name === undefined || last_name ===undefined){
+
+    if (email === undefined || password === undefined || first_name === undefined || last_name === undefined) {
         return res.sendStatus(400);
     }
-    const registerTenant = await req.models.register.createTenant
-    (email,
-    password,
-    first_name,
-    last_name,
-    );
+    const tenants = await req.models.tenant.loginFetchTenantByEmail(email);
+    if (tenants.length === 0) {
+        console.log(`No tenants matched the email: ${email}, creating account`);
+        const registerTenant = await req.models.register.createTenant
+            (email,
+                password,
+                first_name,
+                last_name);
 
-    res.status(201).json(registerTenant);
-    next();
- });
- 
- //to create a landlord
- router.post('/landlord', async (req, res, next) => {
+        res.status(201).json(registerTenant);
+        next();
+    }
+    else if(tenants.length > 0)
+    {
+        console.log(`Tenant account already created with email address = ${email}`);
+        return res.sendStatus(400);
+    }
+    else
+    {
+        console.log(`Unknown error has occured`);
+        return res.sendStatus(401);
+    }
+});
+
+//to create a landlord
+router.post('/landlord', async (req, res, next) => {
 
     let email = req.body.email;
     let password = req.body.password
     let first_name = req.body.first_name
     let last_name = req.body.last_name
 
-    
-    if (email === undefined || password === undefined || first_name === undefined || last_name ===undefined) {
+
+    if (email === undefined || password === undefined || first_name === undefined || last_name === undefined) {
         return res.sendStatus(400);
     }
-    const registerLandlord = await req.models.register.createLandlord
-    (email,
-    password,
-    first_name,
-    last_name,
-    );
 
-    res.status(201).json(registerLandlord);
-    next();
- });
+    const landlords = await req.models.landlord.loginFetchLandlordByEmail(email);
+    if (landlords.length === 0) {
+        console.log(`No landlords matched the email: ${email}, creating account`);
+        const registerLandlord = await req.models.register.createLandlord
+            (email,
+                password,
+                first_name,
+                last_name);
 
- module.exports = router;
+        res.status(201).json(registerLandlord);
+        next();
+    }
+    else if(landlords.length > 0)
+    {
+        console.log(`Landlord account already created with that email address = ${email}`);
+        return res.sendStatus(400);
+    }
+    else
+    {
+        console.log(`Unknown error has occured`);
+        return res.sendStatus(401);
+    }
+
+
+});
+
+module.exports = router;
