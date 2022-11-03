e35634ff962a3b8ed6c4e751ae09021b8626fff9
lukedemaio <demaioluke22@gmail.com>
2022-11-03 15:51:57 -0500

    Update workorder.js

--- backend/models/workorder.js
+++ backend/models/workorder.js
@@ -1,6 +1,15 @@
 const knex = require('../database/knex');
 const WORK_ORDER_TABLE = 'work_order';
-   
+const res = require("express/lib/response");
+
+const createWorkOrder = async (property, tenant, descrip) => {
+    const result = await knex(WORK_ORDER_TABLE).insert({
+        description: descrip,
+        tenant_id: tenant,
+        prop_id: property
+    });
+    return result;
+}
    const workOrderByID = async (wo_num) => {
       //uses passed in id to get the associated tenant but only the specified columns
        const query = knex(WORK_ORDER_TABLE).where({ wo_num })
