209b24990989932563ff3445f2a0e839a1ed05dd
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-16 03:33:13 -0600

    Actually created routes 1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import {Router} from './utils/routes';
+import {BrowserRouter} from 'react-router-dom';
 
 
 
-export const App = () => <Router />;
+export const App = () =>
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+
