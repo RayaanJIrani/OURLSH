6a28b630ca50766ff60708c470e70944c90037c4
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-16 03:53:05 -0600

    Added Scroll to top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-import {Router} from './utils/routes';
+import {Router, ScrollToTop} from './utils';
 import {BrowserRouter} from 'react-router-dom';
 
 
 
 export const App = () =>
     <BrowserRouter>
+        <ScrollToTop />
         <Router />
     </BrowserRouter>
 
