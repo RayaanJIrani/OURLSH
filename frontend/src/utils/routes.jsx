b55dcdde0860144b8fc1a87f9ebdd90e4c98549e
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-16 03:41:53 -0600

    Finished with Routes

--- frontend/src/utils/routes.jsx
+++ frontend/src/utils/routes.jsx
@@ -2,12 +2,13 @@ import {LoginPage} from "../login/login";
 import {TenantProfile} from "../profile/tenantProfile";
 import {LandlordProfile} from "../profile/landlordProfile";
 import {RegisterProfile} from "../register/RegisterProfile";
-import {Route, Routes } from "react-router-dom";
 import { Navigate, useRoutes } from 'react-router-dom';
 
 export const Router = () => {
+    //TODO: As more pages are added, these routes will need to be updated. Have Ray Irani do this.
     return useRoutes([
         { path: '/', element: <LoginPage /> },
+        { path: '/login', element: <LoginPage /> },
         { path: '/tenant', element: <TenantProfile /> },
         { path: '/landlord', element: <LandlordProfile /> },
         { path: '/register', element: <RegisterProfile /> },
