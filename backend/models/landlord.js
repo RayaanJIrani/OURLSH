8429a9eb047600cc28eaffb145fe278312ff85c7
wesoa012 <wesa@smu.edu>
2022-11-16 18:05:06 -0600

    Update landlord by id working

--- backend/models/landlord.js
+++ backend/models/landlord.js
@@ -3,15 +3,15 @@ const LANDLORD_TABLE = 'landlord';
 
 const fetchLandlordByID = async (id) => {
     //uses passed in id to get the associated tenant but only the specified columns
-     const query = knex(LANDLORD_TABLE).where({ id })
-     .select("id",
-             "email",
-             "first_name",
-             "last_name");
+    const query = knex(LANDLORD_TABLE).where({ id })
+        .select("id",
+            "email",
+            "first_name",
+            "last_name");
 
-     const results = await query;
-     return results;
- }
+    const results = await query;
+    return results;
+}
 
 const loginFetchLandlordByEmail = async (email) => {
     //uses passed in id to get the associated tenant but only the specified columns
@@ -19,7 +19,32 @@ const loginFetchLandlordByEmail = async (email) => {
     const results = await query;
     return results;
 }
+
+const updateLandlordById = async (id, email, first_name, last_name, photo) => {
+
+    if (email !== undefined) {
+        const query = knex(LANDLORD_TABLE).update({ email }).where({ id });
+        const results = await query;
+    }
+    if (first_name !== undefined) {
+        const query = knex(LANDLORD_TABLE).update({ first_name }).where({ id });
+        const results = await query;
+    }
+    if (last_name !== undefined) {
+        const query = knex(LANDLORD_TABLE).update({ last_name }).where({ id });
+        const results = await query;
+    }
+    if (photo !== undefined) {
+        const query = knex(LANDLORD_TABLE).update({ photo }).where({ id });
+        const results = await query;
+    }
+    const query = knex(LANDLORD_TABLE).where({ id });
+    const results = await query;
+    return results;
+}
+
 module.exports = {
     fetchLandlordByID,
-    loginFetchLandlordByEmail
+    loginFetchLandlordByEmail,
+    updateLandlordById
 }
