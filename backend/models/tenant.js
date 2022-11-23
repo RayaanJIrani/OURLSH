73a8aa9a13a5ac5c00f7f866d669ac7b2df2ccfd
wesoa012 <wesa@smu.edu>
2022-11-23 11:00:06 -0600

    Table interaction for the get "/tenants" route
    one returns all, one returns all with specified landlord id

--- backend/models/tenant.js
+++ backend/models/tenant.js
@@ -54,8 +54,40 @@ const updateTenantById = async (id, email, first_name, last_name, photo) => {
    return results;
 }
 
+const getTenantsByLandlord = async (landlord_id) => {
+   const query = knex(TENANT_TABLE).where({ landlord_id }).select(
+      "id",
+      "email",
+      "first_name",
+      "last_name",
+      "prop_id",
+      "landlord_id");
+   console.log(`Grabbing all tenants with landlord id = ${landlord_id}...`);
+   console.log(`I have the tenants with landlord id = ${landlord_id} -`);
+   const results = await query;
+   console.log(results);
+   return results;
+}
+
+const getAllTenants = async () => {
+   const query = knex(TENANT_TABLE).select(
+      "id",
+      "email",
+      "first_name",
+      "last_name",
+      "prop_id",
+      "landlord_id");
+   console.log("Grabbing all tenants...");
+   const results = await query;
+   console.log("I all the have the tenants -");
+   console.log(results);
+   return results;
+}
+
 module.exports = {
    fetchTenantByID,
    loginFetchTenantByEmail,
-   updateTenantById
+   updateTenantById,
+   getTenantsByLandlord,
+   getAllTenants
 }
