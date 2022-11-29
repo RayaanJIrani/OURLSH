131151adf020019edab24f5080c82e1d9bc34fdd
PaigeWeng <peiqiw@smu.edu>
2022-11-29 16:18:36 -0600

    u

--- frontend/src/payment/payment.jsx
+++ frontend/src/payment/payment.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { checkTenantAccount } from "../api/UserApi";
 
 export const PaymentPage = () => {
+
+  
   // const handleCacnelClick = () => {
   //   };
   return (
@@ -10,67 +12,97 @@ export const PaymentPage = () => {
       <h1 className="container bg-white mt-5 p-5 display-5 fw-bold ls-tight rounded ">
         New Payment
       </h1>
-      <div class="container p-5">
-        <div class="card px-4">
-          <p class="h8 py-3 fs-5">
+      <div className="container p-5">
+        <div className="card px-4">
+          <p className="h8 py-3 fs-5">
             <strong>Payment Info</strong>
           </p>
-          <div class="row gx-3">
-            <div class="col-12">
-              <div class="d-flex flex-column">
-                <p class="text mb-1">Person Name</p>
+          <div className="row gx-3">
+            <div className="col-6">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Tenant ID</p>
                 <input
-                  class="form-control mb-3"
+                  className="form-control mb-3"
+                  type="text" 
+                />
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Invoice ID</p>
+                <input
+                  className="form-control mb-3 pt-2 "
+                  type="text" 
+                />
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Amount</p>
+                <input
+                  className="form-control mb-3"
+                  type="text" 
+                  placeholder="$"
+                />
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Person Name</p>
+                <input
+                  className="form-control mb-3"
                   type="text"
                   placeholder="Name"
                   //   value="Barry Allen"
                 />
               </div>
             </div>
-            <div class="col-12">
-              <div class="d-flex flex-column">
-                <p class="text mb-1">Card Number</p>
+            <div className="col-12">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Card Number</p>
                 <input
-                  class="form-control mb-3"
+                  className="form-control mb-3"
                   type="text"
                   placeholder="1234 5678 435678"
                 />
               </div>
             </div>
-            <div class="col-6">
-              <div class="d-flex flex-column">
-                <p class="text mb-1">Expiry</p>
+            <div className="col-6">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">Expiry</p>
                 <input
-                  class="form-control mb-3"
+                  className="form-control mb-3"
                   type="text"
                   placeholder="MM/YYYY"
                 />
               </div>
             </div>
-            <div class="col-6">
-              <div class="d-flex flex-column">
-                <p class="text mb-1">CVV/CVC</p>
+            <div className="col-6">
+              <div className="d-flex flex-column">
+                <p className="text mb-1">CVV/CVC</p>
                 <input
-                  class="form-control mb-3 pt-2 "
+                  className="form-control mb-3 pt-2 "
                   type="password"
                   placeholder="***"
                 />
               </div>
             </div>
 
-            <div class="col-6 mb-3 mt-3">
-              <div class="d-flex flex-column">
-                <div class="btn btn-primary mb-3">
-                  <span class="">Pay Now</span>
-                  <span class="fas fa-arrow-right"></span>
+            <div className="col-6 mb-3 mt-3">
+              <div className="d-flex flex-column">
+                <div className="btn btn-primary mb-3">
+                  <span className="">Pay Now</span>
+                  <span className="fas fa-arrow-right"></span>
                 </div>
               </div>
             </div>
-            <div class="col-6 mb-3 mt-3">
-              <div class="d-flex flex-column">
-                <div class="btn btn-warning mb-3">
-                  <span class="">Cancel</span>
-                  <span class="fas fa-arrow-right"></span>
+            <div className="col-6 mb-3 mt-3">
+              <div className="d-flex flex-column">
+                <div className="btn btn-warning mb-3">
+                  <span className="">Cancel</span>
+                  <span className="fas fa-arrow-right"></span>
                 </div>
               </div>
             </div>
