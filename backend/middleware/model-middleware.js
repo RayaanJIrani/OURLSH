1db8c4778fe966cccfb4dab10a72d0f9f7a5ea15
wesoa012 <wesa@smu.edu>
2022-11-29 13:41:20 -0600

    adding payment to middleware

--- backend/middleware/model-middleware.js
+++ backend/middleware/model-middleware.js
@@ -3,13 +3,15 @@ const Register = require('../models/register');
 const Landlord = require('../models/landlord');
 const Login = require('../models/login');
 const WorkOrder = require('../models/workorder');
+const payment = require('../models/payment');
 const createModelsMiddleware = async (req, res, next) => {
    req.models = {
       tenant: Tenant,
       register: Register,
       landlord: Landlord,
       login: Login,
-      workorder: WorkOrder
+      workorder: WorkOrder,
+      payment: Payment
   }
   next();
 }
