7899f3ab8488becf473728ce206ac3d8e6b94049
wesoa012 <wesa@smu.edu>
2022-11-29 23:18:12 -0600

    Update model-middleware.js

--- backend/middleware/model-middleware.js
+++ backend/middleware/model-middleware.js
@@ -3,7 +3,7 @@ const Register = require('../models/register');
 const Landlord = require('../models/landlord');
 const Login = require('../models/login');
 const WorkOrder = require('../models/workorder');
-const payment = require('../models/payment');
+const Payment = require('../models/payment');
 const Invoice = require('../models/invoice'); 
 const createModelsMiddleware = async (req, res, next) => {
    req.models = {
