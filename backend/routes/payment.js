eeac0d97e6b8b374be019f00484ed72bc90255d5
wesoa012 <wesa@smu.edu>
2022-11-30 14:50:36 -0600

    Get payments for specific tenant
    not my idea for a backend endpoint
    
    "/payments/tenants/:id"
    
    only lets either tenant or landlord linked to that tenant access

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -12,7 +12,6 @@ router.post('/', async (req, res, next) => {
 
     if(res.status == 200)
     {
-        const invoice_id = req.data.invoice_id
         const amount = req.data.amount
         const person_name = req.data.person_name
         const card_number = req.data.card_number
@@ -20,15 +19,41 @@ router.post('/', async (req, res, next) => {
         const security_code = req.data.security_code
 
         //make sure fields are valid
-        if (tenant_id === undefined || invoice_id === undefined || amount === undefined || person_name === undefined 
+        if (tenant_id === undefined || amount === undefined || person_name === undefined 
             || card_number === undefined || expiry === undefined || security_code === undefined) {
-                console.log(tenant_id, invoice_id, amount, person_name, card_number, expiry, security_code)
+                console.log(tenant_id, amount, person_name, card_number, expiry, security_code)
             return res.sendStatus(406);
         }
 
         const payment = await req.models.payment.postPayment(tenant_id, invoice_id, amount, person_name, card_number, expiry, security_code);
         res.json(payment);
         res.sendStatus(201);
+        next();
+    }
+    else
+    {   
+        res.sendStatus(res.status);
+    }
+});
+
+//Not my idea for an endpoint
+
+router.get('/tenants/:id', async (req, res, next) => {
+    const params = req.query;
+    const tenant_id = params.id
+    const tenantByID = await req.models.tenant.fetchTenantByID(tenant_id);
+    let auth = await authenticateMultipleClaims(['tenant', `${tenant_id}`], req, res)
+
+    //lets either tenant with id matching access or the landlord linked to that tenant
+    if(res.status != 200)
+    {
+        let auth = await authenticateMultipleClaims(['landlord', `${tenantByID.landlord_id}`], req, res)
+    }
+    if(res.status == 200)
+    {
+        const payments = await req.models.payment.getPayments(tenant_id);
+        res.json(payments);
+        res.sendStatus(200);
         next();
     }
     else
